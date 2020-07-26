88cbc9f8b2bd7a732881c064e1714091b48715a8
siavash <sia1368@gmail.com>
2020-07-26 13:30:29 +0430

    Update student_management.py

--- student_management.py
+++ student_management.py
@@ -4,9 +4,31 @@ from tkinter import *
 root = Tk()
 root.title("student management")
 
+#user name label and entry
 username_var=StringVar()
 label = Label( root, textvariable=username_var, relief=RAISED )
+username_var.set("user name:")
 
-username_var.set("Hey!? How are you doing?")
-label.pack()
+entry_var=StringVar()
+entry=Entry(root,textvariable=entry_var)
+
+label.grid(row=0,column=0)
+entry.grid(row=0,column=1)
+
+#password label and entry
+pass_var=StringVar()
+label_pass = Label( root, textvariable=pass_var, relief=RAISED )
+pass_var.set("password:")
+
+entry_var=StringVar()
+entry_pass=Entry(root,textvariable=entry_var)
+
+label_pass.grid(row=1,column=0)
+entry_pass.grid(row=1,column=1)
+#login button
+login_btn = Button(root, text = 'log in', bd = '5', command = root.destroy)
+login_btn.grid(row=2,column=0)
+#register button
+register_btn = Button(root, text = 'register', bd = '5', command = root.destroy)
+register_btn.grid(row=2,column=1)
 root.mainloop()
